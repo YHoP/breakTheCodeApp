430a0a6c76a99710a811fabf1d460478fa2d4bf3
chitra & yvonne peng <student@epicodus.com>
2015-10-19 16:27:51 -0700

    add sparkly fonts to nonProfitActivity

--- app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
+++ app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
@@ -1,9 +1,13 @@
 package com.epicodus.breakthecode;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
 
 public class NonProfitActivity extends AppCompatActivity {
 
@@ -11,6 +15,11 @@ public class NonProfitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_profit);
+
+        TextView nameLabel = (TextView) findViewById(R.id.nameLabel);
+        Typeface fontSparkly = Typeface.createFromAsset(getAssets(), "fonts/sparkly.ttf");
+        nameLabel.setTypeface(fontSparkly);
+
     }
 
 }
